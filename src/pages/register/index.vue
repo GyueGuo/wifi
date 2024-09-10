c29099a709e056c27ead017db903e383b900f162
guojy01 <guojy01@rabyte.cn>
2024-09-10 20:57:55 +0800

    t

--- src/pages/register/index.vue
+++ src/pages/register/index.vue
@@ -1,5 +1,39 @@
 <template>
 	<view class="container">
-		<text>register</text>
+		
+		<form @submit="formSubmit" @reset="formReset">
+				<view class="uni-form-item uni-column">
+					<view class="title">switch</view>
+					<view>
+						<switch name="switch" />
+					</view>
+				</view>
+		</form>
 	</view>
 </template>
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		methods: {
+			formSubmit: function(e) {
+				console.log('form发生了submit事件，携带数据为：' + JSON.stringify(e.detail.value))
+				var formdata = e.detail.value
+				uni.showModal({
+					content: '表单数据内容：' + JSON.stringify(formdata),
+					showCancel: false
+				});
+			},
+			formReset: function(e) {
+				console.log('清空数据')
+			}
+		}
+	}
+</script>
+<style lang="scss">
+.uni-form-item .title {
+		padding: 20rpx 0;
+	}
+</style>
